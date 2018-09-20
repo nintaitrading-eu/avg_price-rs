c432d3ebf8d116240806f6c93ac00ad24c7a3cc4
Andy Nagels <thereisanewway@gmail.com>
2018-09-20 13:49:56 +0200

    Flush.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io;
+use std::io::Write;
+
 fn main()
 {
-    println!("Hello, world!");
+    print!("S1: ");
+    io::stdout().flush();
+    let mut s1 = String::new();
+    io::stdin().read_line(&mut s1)
+        .expect("Failed to read s1.");
+    println!("S1 = {}", s1);
 }
