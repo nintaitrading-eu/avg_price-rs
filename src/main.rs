10ef478cc2f7d9801918e5a2e06727303124ed78
Andy Nagels <thereisanewway@gmail.com>
2018-09-20 16:45:54 +0200

    Cleanup as preparation for the real implementation.

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ const USAGE: &'static str = "
 avg_price
 
 Usage:
-  avg_price --test=<astr>
+  avg_price --list=<astr>
   avg_price (-h | --help)
   avg_price --version
 
 Options:
   -h --help  Show usage info.
   --version  Show version.
-  --test=<astr>  Print the given test-string.
+  --list=<astr>  List of shares with prices in the form \"s1,p1;s2,p2\"
 ";
 
 fn main()
@@ -23,15 +23,15 @@ fn main()
     let args = Docopt::new(USAGE)
         .and_then(|dopt| dopt.parse())
         .unwrap_or_else(|e| e.exit());
-    println!("{:?}", args);
-    println!("{:}", args.get_str("--test"));
+    println!("{:?}", args); // TODO: Remove after DEBUG
+    println!("Given list: {:}", args.get_str("--list")); // TODO: Remove after DEBUG
 
-    /*print!("S1: ");
     io::stdout().flush();
-    let mut s1 = String::new();
-    io::stdin().read_line(&mut s1)
-        .expect("Failed to read s1.");
-    let s1: u32 = s1.trim().parse()
-        .expect("Error: NaN.");
-    println!("S1 = {}", s1);*/
+    let mut list = args.get_str("--list");
+    // TODO: Parse string: s1,p1
+    // TODO: Parse element s1 p1 en multiply
+    // TODO: calculate average
+    //print!("Result = {}", );
+    //let s1: u32 = s1.trim().parse()
+    //    .expect("Error: NaN.");
 }
