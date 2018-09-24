beca37f2106b1845cb2f67819a6cf5bf28b6b47d
Andy Nagels <thereisanewway@gmail.com>
2018-09-24 16:02:13 +0200

    Code cleanup + final corrections.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 extern crate docopt;
 
-use std::io;
-use std::io::Write;
 use docopt::Docopt;
 
 const USAGE: &'static str = "
@@ -23,34 +21,28 @@ fn main()
     let args = Docopt::new(USAGE)
         .and_then(|dopt| dopt.parse())
         .unwrap_or_else(|e| e.exit());
-    println!("{:?}", args); // TODO: Remove after DEBUG
-    println!("Given list: {:}", args.get_str("--list")); // TODO: Remove after DEBUG
-    io::stdout().flush();
     
     struct SharesPrice
     {
         shares: i32,
-        price: f64,
+        price: f64
     }
 
-    let list = args.get_str("--list");
-    let split_list = list.split(";");
     let mut vec: Vec<SharesPrice> = vec![];
-    for sp in split_list
+    for sp in args.get_str("--list").split(";")
     {
-        let mut split_sp = sp.split(",");
-        let vec_tmp: Vec<&str> = split_sp.collect::<Vec<&str>>();
+        let vec_tmp: Vec<&str> = sp.split(",").collect::<Vec<&str>>();
         let s: i32 = vec_tmp[0].parse::<i32>().unwrap();
         let p: f64 = vec_tmp[1].parse::<f64>().unwrap();
-         
-        let mut struct_sp = SharesPrice { shares: s, price: p}; 
+        let struct_sp = SharesPrice { shares: s, price: p}; 
         vec.push(struct_sp);
     }
-    for (i, item) in vec.iter().enumerate()
+    let mut enumerator: f64 = 0.0;
+    let mut denominator: i32 = 0;
+    for (_i, item) in vec.iter().enumerate()
     {
-        println!("TEST2 :: vector element {} [{}, {}]", i + 1, item.shares, item.price);
+        enumerator = enumerator + (item.shares as f64) * item.price;
+        denominator = denominator + item.shares;
     }
-    // TODO: make the calculation
-    // TODO: refactor
-    //print!("Result = {}", );
+    print!("Result = {}/{} = {}", enumerator, denominator, enumerator/(denominator as f64));
 }
