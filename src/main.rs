9b81eba873bac2ed927e6530f7922da3c1c83632
Andy Nagels <thereisanewway@gmail.com>
2018-09-24 15:35:38 +0200

    List (strings) to split values to converted elements to struct. Added to vector.

--- src/main.rs
+++ src/main.rs
@@ -25,20 +25,32 @@ fn main()
         .unwrap_or_else(|e| e.exit());
     println!("{:?}", args); // TODO: Remove after DEBUG
     println!("Given list: {:}", args.get_str("--list")); // TODO: Remove after DEBUG
-
     io::stdout().flush();
-    let mut list = args.get_str("--list");
-    let mut split = list.split(";");
-    for s in split
+    
+    struct SharesPrice
     {
-        println!("{}", s);
+        shares: i32,
+        price: f64,
     }
-    //let vec: Vec<&str> = split.collect();
 
-    // TODO: Parse string: s1,p1
-    // TODO: Parse element s1 p1 en multiply
-    // TODO: calculate average
+    let list = args.get_str("--list");
+    let split_list = list.split(";");
+    let mut vec: Vec<SharesPrice> = vec![];
+    for sp in split_list
+    {
+        let mut split_sp = sp.split(",");
+        let vec_tmp: Vec<&str> = split_sp.collect::<Vec<&str>>();
+        let s: i32 = vec_tmp[0].parse::<i32>().unwrap();
+        let p: f64 = vec_tmp[1].parse::<f64>().unwrap();
+         
+        let mut struct_sp = SharesPrice { shares: s, price: p}; 
+        vec.push(struct_sp);
+    }
+    for (i, item) in vec.iter().enumerate()
+    {
+        println!("TEST2 :: vector element {} [{}, {}]", i + 1, item.shares, item.price);
+    }
+    // TODO: make the calculation
+    // TODO: refactor
     //print!("Result = {}", );
-    //let s1: u32 = s1.trim().parse()
-    //    .expect("Error: NaN.");
 }
